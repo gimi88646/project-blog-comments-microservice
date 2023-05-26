78602d17d84ecc160e05ac2f7ed06d9757305648
Ghulam Muhammad <ghulammuhammadtunio@gmail.com>
2023-05-27 00:41:27 +0500

    fixed bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
 
   const { content } = req.body;
@@ -27,16 +27,35 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[req.params.id] = comments;
 
-  axios.post("http://localhost:4005", {
-    type: "commentCreated",
+  // send CommentCreated event to event-bus.
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
     data: {
-      commentId: id,
-      comment: content,
+      id,
+      content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
+});
+
+app.post("/events", (req, res) => {
+  // console.log("event received", req.body.type);
+  const { type, data } = req.body;
+  if (type === "CommentModerated") {
+    const { postId, id, status } = body;
+    const comment = commentsByPostId[postId].find(
+      (comment) => comment.id == id
+    );
+    comment.status = status;
+    axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data,
+    });
+  }
+  res.send({});
 });
 
 app.listen(4001, () => {
